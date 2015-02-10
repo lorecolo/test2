d0c0a6aca686f4a37597bedaf0325cccab030c46
lorecolo <lorenzo.colonniello@insiel.it>
2015-02-10 16:56:16 +0100

    prova 3

--- git_test_share_egitt/src/test/testMain.java
+++ git_test_share_egitt/src/test/testMain.java
@@ -5,9 +5,11 @@ public class testMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("aaaaaaaaaadddiiiia zzzz ddddeeeeeee zzzz");
+		System.out.print("aaaaaaaaaadddiiiia zzzz ddddeeeeeee zzzz ");
 		// prova 2
 	 
+		
+		// test prova 3
 		
 	}
 
